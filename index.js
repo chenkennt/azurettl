4589135954b4cb2dcaa69496cdd966c443746b3e
Ken Chen <kenchen@microsoft.com>
2020-09-29 14:45:29 +0800

    format code

--- index.js
+++ index.js
@@ -4,87 +4,91 @@ const armSubscriptions = require('@azure/arm-subscriptions');
 const { daysAgo, deleteResourceById, listResources, listResourcesOnResourceGroup, listResourceGroups } = require("./utils");
 const { Environment } = require("@azure/ms-rest-azure-env");
 async function doCleanup(subsId, subsName, ttl, client, secret, tenant) {
-    let cred = await msRestNodeAuth.loginWithServicePrincipalSecret(client, secret, tenant, {
-      environment: Environment.AzureCloud
-    });
-    // do a subscription check first, to ensure resources doesn't get cleaned up by accident
-    let subscriptionClient = new armSubscriptions.SubscriptionClient(cred);
-    let sub = await subscriptionClient.subscriptions.get(subsId);
-    if (sub.displayName !== subsName) throw `Subscription does not match! Expected subscription: ${subsName}, actual subscription: ${sub.displayName}`;
+  let cred = await msRestNodeAuth.loginWithServicePrincipalSecret(client, secret, tenant, {
+    environment: Environment.AzureCloud
+  });
 
-    console.log(`Start cleaning resources in subscription: ${subsName} (${subsId}), delete resources created over ${ttl} days.`);
-    let resourceClient = new armResources.ResourceManagementClient(cred, subsId);
-    let resources = await listResources(resourceClient);
-    let stats = {
-        totalResources: resources.length,
-        toDeleteResources: 0,
-        deletedResources: 0,
-        totalGroups: 0,
-        toDeleteGroups: 0,
-        deletedGroups: 0
-    };
-    let now = new Date();
-    // delete newly created resources first to solve dependency issues
-    for (let r of resources.sort((x, y) => y.createdTime - x.createdTime)) {
-        let daysCreate = daysAgo( r.createdTime);
-        console.log(`Processing ${r.id}`);
-        if (daysCreate <= ttl) {
-            console.log(`  Created ${daysCreate} day(s) ago, skip.`);
-            continue;
-        }
-        try {
-            stats.toDeleteResources++;
-            console.log(`  Created ${daysCreate} day(s) days ago, deleting...`);
-            await deleteResourceById(resourceClient, r.type, r.id);
-            console.log('  Deleted.');
-            stats.deletedResources++;
-        } catch (err) {
-            if (err.statusCode) {
-                console.log(`  Failed. HTTP status code: ${err.statusCode}, error code: ${err.code}, error message:`);
-                console.log('    ' + (err.body.message || err.body.Message));
-            } else {
-                console.log('  Failed. ' + err);
-            }
-        }
+  // do a subscription check first, to ensure resources doesn't get cleaned up by accident
+  let subscriptionClient = new armSubscriptions.SubscriptionClient(cred);
+  let sub = await subscriptionClient.subscriptions.get(subsId);
+  if (sub.displayName !== subsName) throw `Subscription does not match! Expected subscription: ${subsName}, actual subscription: ${sub.displayName}`;
+
+  console.log(`Start cleaning resources in subscription: ${subsName} (${subsId}), delete resources created over ${ttl} days.`);
+  let resourceClient = new armResources.ResourceManagementClient(cred, subsId);
+  let resources = await listResources(resourceClient);
+  let stats = {
+    totalResources: resources.length,
+    toDeleteResources: 0,
+    deletedResources: 0,
+    totalGroups: 0,
+    toDeleteGroups: 0,
+    deletedGroups: 0
+  };
+
+  let now = new Date();
+  // delete newly created resources first to solve dependency issues
+  for (let r of resources.sort((x, y) => y.createdTime - x.createdTime)) {
+    let daysCreate = daysAgo(r.createdTime);
+    console.log(`Processing ${r.id}`);
+    if (daysCreate <= ttl) {
+      console.log(`  Created ${daysCreate} day(s) ago, skip.`);
+      continue;
     }
-
-    // delete groups which become empty after cleanup
-    for (let rg of (await listResourceGroups(resourceClient))) {
-        const g = rg.name;
-        stats.totalGroups++;
-        console.log(`Processing resource group: ${g}...`);
-        const rc = (await listResourcesOnResourceGroup(resourceClient, g)).length;
-        if (rc) {
-            console.log(`  ${rc} resources in this group, skip.`);
-            continue;
-        }
-        if (0 == rc) {
-            try {
-                stats.toDeleteGroups++;
-                console.log('  No resources in this group, deleting...');
-                await resourceClient.resourceGroups.deleteMethod(g);
-                console.log('  Deleted');
-                stats.deletedGroups++;
-            } catch (err) {
-                if (err.statusCode) {
-                    console.log(`  Failed. HTTP status code: ${err.statusCode}, error code: ${err.code}, error message:`);
-                    console.log('    ' + (err.body.message || err.body.Message));
-                } else {
-                    console.log('  Failed. ' + err);
-                }
-            }
-        }
+    try {
+      stats.toDeleteResources++;
+      console.log(`  Created ${daysCreate} day(s) days ago, deleting...`);
+      await deleteResourceById(resourceClient, r.type, r.id);
+      console.log('  Deleted.');
+      stats.deletedResources++;
+    } catch (err) {
+      if (err.statusCode) {
+        console.log(`  Failed. HTTP status code: ${err.statusCode}, error code: ${err.code}, error message:`);
+        console.log('    ' + (err.body.message || err.body.Message));
+      } else {
+        console.log('  Failed. ' + err);
+      }
     }
+  }
 
-    console.log(`Cleanup completed in ${(new Date() - now) / 1000} seconds, summary:`);
-    console.log(`  Resource processed: ${stats.totalResources}`);
-    console.log(`  Resource can be deleted: ${stats.toDeleteResources}`);
-    console.log(`  Resource deleted: ${stats.deletedResources}`);
-    console.log(`  Resource failed to delete: ${stats.toDeleteResources - stats.deletedResources}`);
-    console.log(`  Resource group processed: ${stats.totalGroups}`);
-    console.log(`  Resource group can be deleted: ${stats.toDeleteGroups}`);
-    console.log(`  Resource group deleted: ${stats.deletedGroups}`);
-    console.log(`  Resource group failed to delete: ${stats.toDeleteGroups - stats.deletedGroups}`);
+  // delete empty groups
+  for (let rg of (await listResourceGroups(resourceClient))) {
+    const g = rg.name;
+    stats.totalGroups++;
+    console.log(`Processing resource group: ${g}...`);
+    const rc = (await listResourcesOnResourceGroup(resourceClient, g)).length;
+    if (rc) {
+      console.log(`  ${rc} resources in this group, skip.`);
+      continue;
+    } else {
+      try {
+        stats.toDeleteGroups++;
+        console.log('  No resources in this group, deleting...');
+        await resourceClient.resourceGroups.deleteMethod(g);
+        console.log('  Deleted');
+        stats.deletedGroups++;
+      } catch (err) {
+        if (err.statusCode) {
+          console.log(`  Failed. HTTP status code: ${err.statusCode}, error code: ${err.code}, error message:`);
+          console.log('    ' + (err.body.message || err.body.Message));
+        } else {
+          console.log('  Failed. ' + err);
+        }
+      }
+    }
+  }
+
+  console.log(`Cleanup completed in ${(new Date() - now) / 1000} seconds, summary:`);
+  console.log(`  Resource processed: ${stats.totalResources}`);
+  console.log(`  Resource can be deleted: ${stats.toDeleteResources}`);
+  console.log(`  Resource deleted: ${stats.deletedResources}`);
+  console.log(`  Resource failed to delete: ${stats.toDeleteResources - stats.deletedResources}`);
+  console.log(`  Resource group processed: ${stats.totalGroups}`);
+  console.log(`  Resource group can be deleted: ${stats.toDeleteGroups}`);
+  console.log(`  Resource group deleted: ${stats.deletedGroups}`);
+  console.log(`  Resource group failed to delete: ${stats.toDeleteGroups - stats.deletedGroups}`);
+
+  // fail the program if any delete failed
+  if (stats.toDeleteResources !== stats.deletedGroups || stats.toDeleteResources !== stats.deletedGroups) process.exitCode = 1;
 }
 
 const subscriptionId = process.argv[2];
--- utils.js
+++ utils.js
@@ -2,85 +2,85 @@ const apiVersionTree = {};
 let prepareApiVersionsPromise;
 
 function getApiVersions(type) {
-    return apiVersionTree[(type || '').toLowerCase()] || ['2020-06-01'];
+  return apiVersionTree[(type || '').toLowerCase()] || ['2020-06-01'];
 }
 
 async function prepareApiVersions(resourceClient) {
-    const providers = await resourceClient.providers.list();
-    for (const provider of providers) {
-        if (provider.registrationState === 'Registered') {
-            for (const resourceType of provider.resourceTypes) {
-                if (resourceType && resourceType.apiVersions) {
-                    const key = provider.namespace.toLowerCase() + '/' + resourceType.resourceType.toLowerCase();
-                    apiVersionTree[key] = resourceType.apiVersions;
-                }
-            }
+  const providers = await resourceClient.providers.list();
+  for (const provider of providers) {
+    if (provider.registrationState === 'Registered') {
+      for (const resourceType of provider.resourceTypes) {
+        if (resourceType && resourceType.apiVersions) {
+          const key = provider.namespace.toLowerCase() + '/' + resourceType.resourceType.toLowerCase();
+          apiVersionTree[key] = resourceType.apiVersions;
         }
+      }
     }
+  }
 }
 
 async function paginationLoop(func1, func2) {
-    let res = [];
-    let resources = await func1();
-    if (!resources) {
-        return res;
-    }
-    res = [...resources];
-    let nextLink = resources.nextLink;
-    let PAGE = 1;
-    while (nextLink) {
-        resources = await func2(nextLink);
-        res = [...res, ...resources];
-        nextLink = resources.nextLink;
-        PAGE++;
-        if (PAGE > 100) {
-            console.log('Reach limit of 100 pages, skip remaining resources')
-            break;
-        }
-    }
+  let res = [];
+  let resources = await func1();
+  if (!resources) {
     return res;
+  }
+  res = [...resources];
+  let nextLink = resources.nextLink;
+  let page = 1;
+  while (nextLink) {
+    resources = await func2(nextLink);
+    res = [...res, ...resources];
+    nextLink = resources.nextLink;
+    page++;
+    if (page > 100) {
+      console.log('Reach limit of 100 pages, skip remaining resources')
+      break;
+    }
+  }
+  return res;
 }
 
 function listResourcesOnResourceGroup(resourceClient, resourceGroupName) {
-    return paginationLoop(() => resourceClient.resources.listByResourceGroup(resourceGroupName, {expand: 'createdTime,properties,tags'}),
-        (nextLink) => resourceClient.resources.listByResourceGroupNext(nextLink));
+  return paginationLoop(() => resourceClient.resources.listByResourceGroup(resourceGroupName, { expand: 'createdTime,properties,tags' }),
+    (nextLink) => resourceClient.resources.listByResourceGroupNext(nextLink));
 }
 
 async function listResourceGroups(resourceClient) {
-    return paginationLoop(() => resourceClient.resourceGroups.list({expand: 'tags'}),
-        (nextLink) => resourceClient.resourceGroups.listNext(nextLink));
+  return paginationLoop(() => resourceClient.resourceGroups.list({ expand: 'tags' }),
+    (nextLink) => resourceClient.resourceGroups.listNext(nextLink));
 }
 
 async function listResources(resourceClient) {
-    return paginationLoop(() => resourceClient.resources.list({expand: 'createdTime,properties,tags'}),
-        (nextLink) => resourceClient.resources.listNext(nextLink));
+  return paginationLoop(() => resourceClient.resources.list({ expand: 'createdTime,properties,tags' }),
+    (nextLink) => resourceClient.resources.listNext(nextLink));
 }
 
 async function deleteResourceById(resourceClient, type, id) {
-    if (!prepareApiVersionsPromise) {
-        prepareApiVersionsPromise = prepareApiVersions(resourceClient);
+  if (!prepareApiVersionsPromise) {
+    prepareApiVersionsPromise = prepareApiVersions(resourceClient);
+  }
+  await prepareApiVersionsPromise;
+  for (const v of getApiVersions(type)) {
+    try {
+      return await resourceClient.resources.deleteById(id, v, { properties: ['createdTime'] });
+    } catch (e) {
+      if (e.message.includes('The supported api-versions')) {
+        continue;
+      }
+      throw e;
     }
-    await prepareApiVersionsPromise;
-    for (const v of getApiVersions(type)) {
-        try {
-            return await resourceClient.resources.deleteById(id, v, {properties:[ 'createdTime']});
-        } catch (e) {
-            if (e.message.includes('The supported api-versions')) {
-                continue;
-            }
-            throw e;
-        }
-    }
-    throw new Error('all api versions tried');
+  }
+  throw new Error('all api versions tried');
 }
 
 function hoursAgo(date) {
-    const seconds = Math.floor((new Date() - date) / 1000 );
-    return Math.floor( seconds / 3600 );
+  const seconds = Math.floor((new Date() - date) / 1000);
+  return Math.floor(seconds / 3600);
 }
 
 function daysAgo(date) {
-    return Math.floor(hoursAgo(date) / 24);
+  return Math.floor(hoursAgo(date) / 24);
 }
 
 exports.listResources = listResources;
